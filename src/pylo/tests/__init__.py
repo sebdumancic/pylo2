e581d1604be90f539dff8ac4d789ad5f798001b2
Sebastijan Dumancic <sebastijan.dumancic@gmail.com>
2020-11-25 16:21:50 +0100

    fix tests

--- src/pylo/tests/__init__.py
+++ src/pylo/tests/__init__.py
@@ -1,6 +1,18 @@
-from .test_gnuprolog import all_gnu_tests
-from .test_swipy import all_swipl_tests
-from .test_xsb import all_xsb_tests
+try:
+    from .test_gnuprolog import all_gnu_tests
+except Exception:
+    pass
+
+try:
+    from .test_swipy import all_swipl_tests
+except Exception:
+    pass
+
+try:
+    from .test_xsb import all_xsb_tests
+except Exception:
+    pass
+
 from .test_kanren import test_kanren
 from .test_datalog import test_datalog
 from .test_language import test_language
